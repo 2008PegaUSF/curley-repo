143f85bfc1c4355b6a9b3f2cd9435d713c2136f2
vyruss <vyruss@gmail.com>
2020-08-17 14:22:53 -0500

    Initial Commit

---
+++ src/gittest/Greetings.java
@@ -0,0 +1,10 @@
+package gittest;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We are playing the game.");
+	}
+
+}
